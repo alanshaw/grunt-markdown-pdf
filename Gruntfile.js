bbe6413a2503cdd279a3bb30debe96e59a66bb82
Alan Shaw <alan@freestyle-developments.co.uk>
2013-09-04 09:13:01 +0100

    Upgrade to latest maekdown-pdf module

--- Gruntfile.js
+++ Gruntfile.js
@@ -39,20 +39,20 @@ module.exports = function(grunt) {
       tests: ['test/*_test.js']
     }
 
-  });
+  })
 
   // Actually load this plugin's task(s).
-  grunt.loadTasks('tasks');
+  grunt.loadTasks('tasks')
 
   // These plugins provide necessary tasks.
-  grunt.loadNpmTasks('grunt-contrib-clean');
-  grunt.loadNpmTasks('grunt-contrib-nodeunit');
+  grunt.loadNpmTasks('grunt-contrib-clean')
+  grunt.loadNpmTasks('grunt-contrib-nodeunit')
 
   // Whenever the "test" task is run, first clean the "tmp" dir, then run this
   // plugin's task(s), then test the result.
-  grunt.registerTask('test', ['clean', 'markdownpdf', 'nodeunit']);
+  grunt.registerTask('test', ['clean', 'markdownpdf', 'nodeunit'])
 
   // By default, run all tests.
-  grunt.registerTask('default', ['test']);
+  grunt.registerTask('default', ['test'])
   
-};
+}
--- test/markdownpdf_test.js
+++ test/markdownpdf_test.js
@@ -2,7 +2,7 @@ var grunt = require('grunt')
 
 exports.markdownpdf = {
   all: function(test) {
-    test.expect(2);
+    test.expect(2)
 
     var contents0 = grunt.file.read('tmp/all/test0.pdf')
     
@@ -15,21 +15,21 @@ exports.markdownpdf = {
     test.done()
   },
   some0: function(test) {
-    test.expect(1);
+    test.expect(1)
 
     var contents = grunt.file.read('tmp/some0/test0.pdf')
     
     test.ok(contents.length > 0)
 
-    test.done();
+    test.done()
   },
   some1: function(test) {
-    test.expect(1);
+    test.expect(1)
 
     var contents = grunt.file.read('tmp/some1/test1.pdf')
     
     test.ok(contents.length > 0)
 
-    test.done();
+    test.done()
   }
 }
