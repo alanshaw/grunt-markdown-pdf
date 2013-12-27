d258903ba1da923d20891b922d2b77c758bfbb60
Alan Shaw <alan@freestyle-developments.co.uk>
2013-12-27 18:23:49 +0000

    Use latest markdown-pdf module

--- tasks/markdownpdf.js
+++ tasks/markdownpdf.js
@@ -7,22 +7,22 @@
  */
 
 var markdownpdf = require("markdown-pdf")
-  , fs = require("fs")
+
   , path = require("path")
   , async = require("async")
 
 module.exports = function (grunt) {
 
   grunt.registerMultiTask("markdownpdf", "Convert Markdown documents to PDF", function () {
-    
+
     var opts = this.options()
       , done = this.async()
 
     // Create the tasks to process the targets
     var tasks = this.files.map(function (f) {
-      
+
       return function (cb) {
-        
+
         var srcs = f.src.filter(function (filepath) {
           // Warn on and remove invalid source files (if nonull was set).
           if (!grunt.file.exists(filepath)) {
@@ -36,41 +36,29 @@ module.exports = function (grunt) {
           grunt.verbose.writeln("Found src file: " + filepath)
           return true
         })
-        
-        markdownpdf(srcs, opts, function (er, pdfs) {
-          
-          // markdownpdf may return a single file path as a string if only 1 PDF is created
-          pdfs = Array.isArray(pdfs) ? pdfs : [pdfs]
-          
-          // Create the tasks to move the PDFs into the correct directory
-          var tasks = pdfs.map(function (tmpPath, i) {
-            var destPath = path.join(f.dest, path.basename(srcs[i]).replace(/\.(markdown|md)/g, "") + ".pdf")
-            grunt.verbose.writeln("Determined dest path: " + destPath)
-            
-            return function (cb) {
-              grunt.verbose.writeln("Copying from " + tmpPath + " to " + destPath)
-              grunt.file.copy(tmpPath, destPath)
-              cb(null, destPath)
-            }
-          })
-          
-          async.parallel(tasks, function (er, destPaths) {
-            if (er) return cb(er)
-            cb(null, destPaths)
-          })
+
+        var dests = srcs.map(function (src) {
+          var destPath = path.join(f.dest, path.basename(src).replace(/\.(markdown|md)/g, "") + ".pdf")
+          grunt.verbose.writeln("Determined dest path: " + destPath)
+          return destPath
+        })
+
+        markdownpdf(opts).from(srcs).to(dests, function (er) {
+          if (er) return cb(er)
+          cb(null, dests)
         })
       }
     })
-    
+
     async.parallel(tasks, function (er, targetDestPaths) {
       if (er) return grunt.warn(er)
-      
+
       targetDestPaths.forEach(function (destPaths) {
         destPaths.forEach(function(destPath) {
           grunt.log.ok(destPath)
         })
       })
-      
+
       done()
     })
   })
